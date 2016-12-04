4c578d82e92b1497d4277088df8aa8dd6a6a689c
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2016-12-04 19:36:08 +0530

    Changed basic design

--- backend/senders/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SendersConfig(AppConfig):
-    name = 'senders'
---
+++ npsnotify/send/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class notification(models.Model):
+    author = models.ForiegnKey('auth.User')
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    groups = models.TextField()
+    goers = models.TextField()
+
+
+    def publish(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class receipent(models.Model):
+    email = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    grade = models.IntegerField()
+    section = models.CharField(max_length=1)
+    house = models.CharField(max_length=100)
