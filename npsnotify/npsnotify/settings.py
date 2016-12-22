68a00752dcebfb4e2429dc3670ed401a54b98cff
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2016-12-22 17:29:17 +0530

    Update db

--- npsnotify/npsnotify/settings.py
+++ npsnotify/npsnotify/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'npsnotify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'notifydb',
+        'USER': os.environ['NUSER'],
+        'PASSWORD': os.environ['NPASS'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
