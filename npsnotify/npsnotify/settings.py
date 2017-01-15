e3d2ecc4658dbfc2c169006a27f5a430b7b4fb09
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2017-01-15 17:24:08 +0530

    update login api

--- npsnotify/npsnotify/settings.py
+++ npsnotify/npsnotify/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'send'
-    'rest_framework.authtoken'
+    'send',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- npsnotify/send/models.py
+++ npsnotify/send/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
 
 # Create your models here.
 class notification(models.Model):
@@ -24,3 +29,9 @@ class receipent(models.Model):
     grade = models.IntegerField()
     section = models.CharField(max_length=1)
     house = models.CharField(max_length=100)
+
+#create new user tokens
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
