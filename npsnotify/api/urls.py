d3d2d1a51d3d78f32f7fe73303aa825ab087e938
Chinmaya Krishnan Mahesh <chinmaya.mahesh@gmail.com>
2017-02-03 23:31:14 +0530

    Integrated new UI

--- npsnotify/api/urls.py
+++ npsnotify/api/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^send/', views.sendmsg.as_view(), name='sendmsg'),
     url(r'^details/', views.details.as_view(), name='details'),
-    url(r'^getgroups/', views.getgroups.as_view(), name='getgroups'),
 ]
--- npsnotify/api/views.py
+++ npsnotify/api/views.py
@@ -18,16 +18,10 @@ class sendmsg(APIView):
         notif = data["notification"]
         groupl = data["groups"].split(',')
         print("notification:", notif,"groups:", groupl)
-        names = User.objects.all()
+        names = User.objects.filter(groups__name__in=groupl)
         emails = set()
         for i in names:
-            f = 1
-            for j in groupl:
-                if i.groups.filter(name=j).exists() == 0:
-                    f = 0
-                    break
-            if f:
-                emails.add(i.email)
+            emails.add(i.email)
         print(emails)
         sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
         for i in emails:
@@ -53,13 +47,5 @@ class details(APIView):
         for i in group:
             if request.user.groups.filter(name=i).exists() == 0:
                 resp = i
-        jresp = "{type: \"" + resp + "\"}"
+        jresp = "{type: \"" + resp + "\",name:" + request.user.name + "}"
         return Response(jresp, status=status.HTTP_200_OK)
-
-class getgroups(APIView):
-    def get(self, request, *args, **kw):
-        groups = Group.objects.all()
-        gn = []
-        for i in group:
-            gn.append(i.name)
-        return Response(json.dumps(gn), status=status.HTTP_200_OK)
